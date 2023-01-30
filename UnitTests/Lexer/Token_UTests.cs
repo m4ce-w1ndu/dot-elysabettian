2bca58c7a1a2b8dc5340daac9ad2863e62e7d2e5
m4ce-w1ndu <ag.novitskiy@icloud.com>
2023-01-30 02:40:10 +0100

    added sample unit tests

--- UnitTests/Lexer/Token_UTests.cs
+++ UnitTests/Lexer/Token_UTests.cs
@@ -4,6 +4,12 @@ namespace UnitTests.Lexer
 {
     public class Token_UTests
     {
+        /// <summary>
+        /// This test checks that the parametrized constuctor
+        /// actually performs its work. This test is useless,
+        /// but it is written to demonstrate that xUnit is
+        /// properly configured.
+        /// </summary>
         [Fact]
         public void Construct_TokenWithType()
         {
@@ -12,6 +18,20 @@ namespace UnitTests.Lexer
             Assert.Equal(TokenType.If, token.Type);
             Assert.Equal("if", token.Text);
             Assert.Equal(1, token.Line);
+        }
+
+        /// <summary>
+        /// This test checks that the default values are
+        /// effectively confirming an undefined behaviour.
+        /// </summary>
+        [Fact]
+        public void Construct_Empty()
+        {
+            var token = new Token();
+
+            Assert.Equal(TokenType.OpenParen, token.Type);
+            Assert.Null(token.Text);
+            Assert.Equal(0, token.Line);
         }
     }
 }
