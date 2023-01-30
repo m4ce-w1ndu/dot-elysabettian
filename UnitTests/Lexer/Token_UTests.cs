2536784c819c2bef709a3b88a3612ba57d629ab2
m4ce-w1ndu <ag.novitskiy@icloud.com>
2023-01-30 02:36:43 +0100

    Added unit test project

---
+++ UnitTests/Lexer/Token_UTests.cs
@@ -0,0 +1,17 @@
+﻿using Lexer.Common;
+
+namespace UnitTests.Lexer
+{
+    public class Token_UTests
+    {
+        [Fact]
+        public void Construct_TokenWithType()
+        {
+            var token = new Token(TokenType.If, "if", 1);
+
+            Assert.Equal(TokenType.If, token.Type);
+            Assert.Equal("if", token.Text);
+            Assert.Equal(1, token.Line);
+        }
+    }
+}
