3966f300f90aae4ed92358d2c117e4f32396a1be
m4ce-w1ndu <ag.novitskiy@icloud.com>
2023-01-30 01:30:41 +0100

    completed Token implementation

---
+++ Lexer/Common/Token.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexer.Common
+{
+    /// <summary>
+    /// Holds data to represent a Token in the language
+    /// </summary>
+    public struct Token
+    {
+        /// <summary>
+        /// Type of the token
+        /// </summary>
+        public TokenType Type { get; init; }
+        
+        /// <summary>
+        /// Text value of the token
+        /// </summary>
+        public string Text { get; init; }
+
+        /// <summary>
+        /// Line bound to this token
+        /// </summary>
+        public int Line { get; init; }
+
+        /// <summary>
+        /// Constructs a new Token
+        /// </summary>
+        /// <param name="type">Type of the token</param>
+        /// <param name="text">Text value of the token</param>
+        /// <param name="line">Line bound to the token</param>
+        public Token(TokenType type, string text, int line)
+        {
+            Type = type;
+            Text = text;
+            Line = line;
+        }
+    }
+}
