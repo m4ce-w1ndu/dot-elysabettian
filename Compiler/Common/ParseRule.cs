9e6ab07bd26ed81aee95c1fc541f583700ca457b
m4ce-w1ndu <ag.novitskiy@icloud.com>
2023-01-30 13:26:42 +0100

    added ParseRule definition

---
+++ Compiler/Common/ParseRule.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Compiler.Common
+{
+    /// <summary>
+    /// Holds references to functions and precedence value
+    /// useful for parsing phase.
+    /// </summary>
+    public struct ParseRule
+    {
+        /// <summary>
+        /// Prefix parsing function.
+        /// </summary>
+        /// <value>Parsing function for prefix.</value>
+        public Action<bool> Prefix { get; init; }
+
+        /// <summary>
+        /// Infix parsing function.
+        /// </summary>
+        /// <value>Parsing function for infix.</value>
+        public Action<bool> Infix { get; init; }
+
+        /// <summary>
+        /// Parsing precedence enumeration.
+        /// </summary>
+        /// <value>Precedence of parsing phase.</value>
+        public Precedence Precedence { get; init; }
+
+        /// <summary>
+        /// Constructs a new ParseRule
+        /// </summary>
+        /// <param name="prefix">Prefix rule function.</param>
+        /// <param name="infix">Infix rule function.</param>
+        /// <param name="precedence">Parsing precedence value.</param>
+        public ParseRule(Action<bool> prefix, Action<bool> infix, Precedence precedence)
+        {
+            Prefix = prefix;
+            Infix = infix;
+            Precedence = precedence;
+        }
+    }
+}
