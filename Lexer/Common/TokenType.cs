a4670479ddd00233d23a7a6b7fdb6c2f04bdb9d1
m4ce-w1ndu <ag.novitskiy@icloud.com>
2023-01-30 01:27:47 +0100

    added TokenType enum

---
+++ Lexer/Common/TokenType.cs
@@ -0,0 +1,33 @@
+﻿namespace Lexer.Common
+{
+    /// <summary>
+    /// Identifies the type of a unique Token
+    /// </summary>
+    public enum TokenType
+    {
+        // Single-character tokens.
+        OpenParen, CloseParen,
+        OpenCurly, CloseCurly,
+        OpenSquare, CloseSquare,
+        Comma, Dot, Semicolon,
+        Tilde, Caret,
+
+        // One or two character tokens.
+        Plus, PlusEqual, Minus, MinusEqual,
+        Star, StarEqual, Slash, SlashEqual,
+        Excl, ExclEqual, Equal, EqualEqual,
+        Greater, GreaterEqual, Less, LessEqual,
+        Amp, AmpAmp, Pipe, PipePipe,
+
+        // Literals.
+        Identifier, String, Number,
+
+        // Keywords.
+        Class, Else, False, Func,
+        For, If, Null, Print, Return,
+        Super, This, True, Var, While,
+
+        Error,
+        Eof,
+    }
+}
