2588ddce029c5c67205e017e830de91692f32a65
m4ce-w1ndu <ag.novitskiy@icloud.com>
2023-02-02 23:51:47 +0100

    added Instance implementation

---
+++ Compiler/Common/Types/Instance.cs
@@ -0,0 +1,28 @@
+﻿namespace Compiler.Common.Types
+{
+    /// <summary>
+    /// Represents an instance of a class.
+    /// </summary>
+    public struct Instance
+    {
+        /// <summary>
+        /// Class that has been instantiated.
+        /// </summary>
+        public Class Class { get; set; }
+
+        /// <summary>
+        /// Fields of this instance.
+        /// </summary>
+        public Dictionary<string, Value> Fields { get; set; }
+
+        /// <summary>
+        /// Constructs a new instance
+        /// </summary>
+        /// <param name="class_">Class to instantiate.</param>
+        public Instance(Class class_)
+        {
+            Class = class_;
+            Fields = new();
+        }
+    }
+}
