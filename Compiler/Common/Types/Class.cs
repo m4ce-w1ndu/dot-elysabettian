90747037cc1478ffd82744b9bbecb390ba9e3b67
m4ce-w1ndu <ag.novitskiy@icloud.com>
2023-02-02 23:49:47 +0100

    added Class type implementation

--- Compiler/Common/Types/Class.cs
+++ Compiler/Common/Types/Class.cs
@@ -1,14 +1,28 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Compiler.Common.Types
 {
+    /// <summary>
+    /// Represents a class in the language.
+    /// </summary>
     public struct Class
     {
+        /// <summary>
+        /// Name of the class.
+        /// </summary>
         public string Name { get; init; }
 
+        /// <summary>
+        /// Methods in the class.
+        /// </summary>
         public Dictionary<string, Closure> Methods { get; init; }
+
+        /// <summary>
+        /// Constructs a new class.
+        /// </summary>
+        /// <param name="name">Name of the class.</param>
+        public Class(string name)
+        {
+            Name = name;
+            Methods = new();
+        }
     }
 }
