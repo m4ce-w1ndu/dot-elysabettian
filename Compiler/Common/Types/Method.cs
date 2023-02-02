90928ea2e28557d824c4e9685d4e882047dd0f7b
m4ce-w1ndu <ag.novitskiy@icloud.com>
2023-02-02 23:54:40 +0100

    added Method implementation

---
+++ Compiler/Common/Types/Method.cs
@@ -0,0 +1,29 @@
+﻿namespace Compiler.Common.Types
+{
+    /// <summary>
+    /// Represents a method.
+    /// </summary>
+    public struct Method
+    {
+        /// <summary>
+        /// Instance of the class bound with this method.
+        /// </summary>
+        public Instance Receiver { get; set; }
+
+        /// <summary>
+        /// Instance of the function.
+        /// </summary>
+        public Closure Function { get; set; }
+
+        /// <summary>
+        /// Constructs a new Method.
+        /// </summary>
+        /// <param name="receiver">Instance of the class to bind.</param>
+        /// <param name="function">Instance of the function.</param>
+        public Method(Instance receiver, Closure function)
+        {
+            Receiver = receiver;
+            Function = function;
+        }
+    }
+}
