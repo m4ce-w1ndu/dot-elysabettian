7722329b709ac571ca5967c210cd9734f50231b0
m4ce-w1ndu <ag.novitskiy@icloud.com>
2023-02-02 23:48:11 +0100

    added closure and list extensions

---
+++ Compiler/Common/Types/Closure.cs
@@ -0,0 +1,31 @@
+﻿using Compiler.Utilities;
+
+namespace Compiler.Common.Types
+{
+    /// <summary>
+    /// Represents a closure.
+    /// </summary>
+    public struct Closure
+    {
+        /// <summary>
+        /// Function associated to this closure.
+        /// </summary>
+        public Function Function { get; set; }
+
+        /// <summary>
+        /// Upvalues in this closure.
+        /// </summary>
+        public List<Upvalue?> Upvalues { get; set; }
+
+        /// <summary>
+        /// Constructs a new Closure
+        /// </summary>
+        /// <param name="function">Function of this closure.</param>
+        public Closure(Function function)
+        {
+            Function = function;
+            Upvalues = new();
+            Upvalues.Resize(function.UpvalueCount, null);
+        }
+    }
+}
---
+++ Compiler/Utilities/ListExtensions.cs
@@ -0,0 +1,36 @@
+﻿namespace Compiler.Utilities
+{
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// Resizes the list.
+        /// </summary>
+        /// <typeparam name="T">Type of the object. Usually inferred.</typeparam>
+        /// <param name="list">List to resize.</param>
+        /// <param name="size">New size.</param>
+        /// <param name="element">Value of the elements to fill.</param>
+        public static void Resize<T>(this List<T> list, int size, T element)
+        {
+            int curr = list.Count;
+            if (size < curr)
+            {
+                list.RemoveRange(curr, size);
+                return;
+            }
+
+            if (size > curr) list.Capacity = size;
+            list.AddRange(Enumerable.Repeat(element, size - curr));
+        }
+
+        /// <summary>
+        /// Resizes the list.
+        /// </summary>
+        /// <typeparam name="T">Type of the object. Usually inferred.</typeparam>
+        /// <param name="list">List to resize.</param>
+        /// <param name="size">New size.</param>
+        public static void Resize<T>(this List<T> list, int size) where T : new()
+        {
+            Resize(list, size, new T());
+        }
+    }
+}
