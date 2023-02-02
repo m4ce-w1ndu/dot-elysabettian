36e613d7cb8a36379a5f63533b02f44a7d603c17
m4ce-w1ndu <ag.novitskiy@icloud.com>
2023-02-03 00:35:54 +0100

    added FunctionType enumeration

---
+++ Compiler/Common/FunctionType.cs
@@ -0,0 +1,13 @@
+﻿namespace Compiler.Common
+{
+    /// <summary>
+    /// Types of functions allowed in the language.
+    /// </summary>
+    public enum FunctionType
+    {
+        Function,
+        NativeFunction,
+        Method,
+        Main
+    }
+}
