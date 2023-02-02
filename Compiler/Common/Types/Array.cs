57b2a2b5eaafb7cd64daea6d69c221afea6d7487
m4ce-w1ndu <ag.novitskiy@icloud.com>
2023-02-03 00:07:09 +0100

    implemented Array type

---
+++ Compiler/Common/Types/Array.cs
@@ -0,0 +1,26 @@
+﻿namespace Compiler.Common.Types
+{
+    /// <summary>
+    /// Implementation of an array type.
+    /// </summary>
+    public class Array
+    {
+        /// <summary>
+        /// List of values.
+        /// </summary>
+        public List<Value> Values;
+
+        /// <summary>
+        /// Number of values.
+        /// </summary>
+        public int Count => Values.Count;
+
+        /// <summary>
+        /// Constructs a new Value.
+        /// </summary>
+        public Array()
+        {
+            Values = new();
+        }
+    }
+}
