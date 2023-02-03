09228cbc6389692fb740be4eb806168cbb38599a
m4ce-w1ndu <ag.novitskiy@icloud.com>
2023-02-03 01:15:21 +0100

    added ClassCompiler implementation

---
+++ Compiler/ClassCompiler.cs
@@ -0,0 +1,28 @@
+﻿namespace Compiler
+{
+    /// <summary>
+    /// Implementation of Class Compiler
+    /// </summary>
+    public class ClassCompiler
+    {
+        /// <summary>
+        /// Internal class compiler
+        /// </summary>
+        public ClassCompiler? Enclosing { get; set; }
+
+        /// <summary>
+        /// Whether it has or it hasn't a superclass.
+        /// </summary>
+        public bool HasSuperclass { get; set; }
+
+        /// <summary>
+        /// Constructs a new ClassCompiler
+        /// </summary>
+        /// <param name="enclosing">Enclosing (internal) class compiler</param>
+        public ClassCompiler(ClassCompiler? enclosing)
+        {
+            Enclosing = enclosing;
+            HasSuperclass = false;
+        }
+    }
+}
