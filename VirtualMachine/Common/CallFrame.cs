a4e9bc582d09cea0cc5ed1b6e881918e56904b3d
m4ce-w1ndu <ag.novitskiy@icloud.com>
2023-02-08 00:34:56 +0100

    added CallFrame implementation

---
+++ VirtualMachine/Common/CallFrame.cs
@@ -0,0 +1,39 @@
+﻿using Compiler.Common.Types;
+
+namespace VirtualMachine.Common
+{
+    /// <summary>
+    /// Holds information of a function call
+    /// stack frame
+    /// </summary>
+    public class CallFrame
+    {
+        /// <summary>
+        /// Function's closure
+        /// </summary>
+        public Closure Closure { get; set; }
+
+        /// <summary>
+        /// Instruction pointer value
+        /// </summary>
+        public int InstructionPtr { get; set; }
+
+        /// <summary>
+        /// Stack pointer value
+        /// </summary>
+        public int StackPtr { get; set; }
+
+        /// <summary>
+        /// Constructs a new CallFrame
+        /// </summary>
+        /// <param name="closure">Function's closure</param>
+        /// <param name="instructionPtr">Instruction pointer value</param>
+        /// <param name="stackPtr">Stack pointer value</param>
+        public CallFrame(Closure closure, int instructionPtr, int stackPtr)
+        {
+            Closure = closure;
+            InstructionPtr = instructionPtr;
+            StackPtr = stackPtr;
+        }
+    }
+}
