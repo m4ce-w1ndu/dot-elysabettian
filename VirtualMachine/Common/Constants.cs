9781fcd92524c724cac241a40461f3664d5fff56
m4ce-w1ndu <ag.novitskiy@icloud.com>
2023-02-08 00:35:13 +0100

    added constants for virtual machine

---
+++ VirtualMachine/Common/Constants.cs
@@ -0,0 +1,17 @@
+﻿using Compiler.Common;
+
+namespace VirtualMachine.Common
+{
+    public static class Constants
+    {
+        /// <summary>
+        /// Max number of stack frames
+        /// </summary>
+        public const int FRAMES_MAX = 64;
+
+        /// <summary>
+        /// Max size of stack
+        /// </summary>
+        public const int STACK_MAX = (FRAMES_MAX * Value.UINT8_COUNT);
+    }
+}
