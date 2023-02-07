ff96b65cfb5497731cc4200298207aa603aa9417
m4ce-w1ndu <ag.novitskiy@icloud.com>
2023-02-08 00:32:33 +0100

    added enumeration for execution results

---
+++ VirtualMachine/Common/ExecutionResult.cs
@@ -0,0 +1,21 @@
+﻿namespace VirtualMachine.Common
+{
+    /// <summary>
+    /// Represents the possible states of execution
+    /// </summary>
+    public enum ExecutionResult
+    {
+        /// <summary>
+        /// Signals an OK execution
+        /// </summary>
+        Ok,
+        /// <summary>
+        /// Signals an error in compilation phase
+        /// </summary>
+        CompileError,
+        /// <summary>
+        /// Signals an error at runtime
+        /// </summary>
+        RuntimeError
+    }
+}
